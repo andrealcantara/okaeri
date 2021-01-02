c81f8af792ec0dfdbb84ac397d935bf0f95a0b43
André Alcantara <3995587+andrealcantara@users.noreply.github.com>
2021-01-02 00:25:38 -0300

    Primeiro Commit
    Signed-off-by: André Alcantara <3995587+andrealcantara@users.noreply.github.com>

---
+++ src/main/java/com/gerador/cpt/okaeri/GeradorOkaeriApplication.java
@@ -0,0 +1,13 @@
+package com.gerador.cpt.okaeri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorOkaeriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorOkaeriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerador/cpt/okaeri/GeradorOkaeriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerador.cpt.okaeri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorOkaeriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
